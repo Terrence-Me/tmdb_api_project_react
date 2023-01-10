87fc65ad8cae29036f3c05c0b7f16842f4ac5c73
Terrence Meikle <terrence_meikle@yahoo.com>
2023-01-10 17:07:21 -0500

    add useState to search bar component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Icon, Input } from 'semantic-ui-react';
 
 const SearchBar = () => {
+  const [searchedMovie, setSearchedMovie] = useState('');
+
+  const onInputChange = (event) => {
+    console.log(event.target.value);
+    setSearchedMovie(event.target.value);
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log(searchedMovie);
+
+    // onFormSubmit(searchedMovie);
+  };
+
   return (
     <div className="ui segment search-bar">
-      <form action="" className="ui form">
+      <form action="" className="ui form" onSubmit={onSubmit}>
         <div className="field">
           <label htmlFor="input">Search Movies</label>
           <Input
             icon={<Icon name="search" inverted circular link />}
             placeholder="Search..."
+            value={searchedMovie}
+            onChange={onInputChange}
           />
         </div>
       </form>
